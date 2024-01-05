8db5613164500f7768c35b512765374646966cca
jasmin <jasminchakma895@gmail.com>
2024-01-05 10:21:16 +0600

    static site genaration build

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,6 +1,8 @@
 
 const page = async() => {
-    const res = await fetch('http://localhost:3004/posts');
+    const res = await fetch('http://localhost:3004/posts', {
+        cache : 'force-cache'
+    });
     const  posts =  await res.json();
     console.log(posts)
     return (
