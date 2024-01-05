58887712eeb28a15f1c560f1d349e77e445191ca
jasmin <jasminchakma895@gmail.com>
2024-01-05 10:19:22 +0600

    fetch data from server

---
+++ src/app/posts/page.js
@@ -0,0 +1,13 @@
+
+const page = async() => {
+    const res = await fetch('http://localhost:3004/posts');
+    const  posts =  await res.json();
+    console.log(posts)
+    return (
+        <div>
+            <h1 className="text-xl">Total Posts : {posts.length}</h1>
+        </div>
+    );
+};
+
+export default page;
